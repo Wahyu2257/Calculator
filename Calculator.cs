5178b52f425564be64d57698ebbec540540c2ed8
kamarudin <kamarudin@amikom.ac.id>
2019-03-12 14:54:42 +0700

    Nambah dua baris kode

--- Calculator.cs
+++ Calculator.cs
@@ -6,7 +6,8 @@ namespace CaclulatorConsoleApp
     {
         static void Main(string[] args)
         {
-            
+            int a = 1 + 2;
+			Console.WriteLine(a);
         }        
     }    
 }
